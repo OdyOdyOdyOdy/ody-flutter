96e3b9f60f64f289eee06d1cb7049caacf0e3b10
haeum808 <159797292+haeum808@users.noreply.github.com>
2024-12-29 19:15:28 +0900

    setting #7: 필요한 색상들 추가

---
+++ ody_flutter/assets/color/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CommonColors {
+  static const Color black = Color(0xFF000000);
+  static const Color cream = Color(0xFFFFFFF6);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color purple_300 = Color(0xFFB8A7FF);
+  static const Color purple_800 = Color(0xFF5C47AE);
+
+  static const Color gray_100 = Color(0xFFF9F9F8);
+  static const Color gray_200 = Color(0xFFF2F2F6);
+  static const Color gray_300 = Color(0xFFE4E4EA);
+  static const Color gray_350 = Color(0xFFD6D6D6);
+  static const Color gray_400 = Color(0xFFB1B1BB);
+  static const Color gray_400_70 = Color(0xB3B1B1BB);
+  static const Color gray_500 = Color(0xFF8E8E93);
+  static const Color gray_600 = Color(0xFF636366);
+  static const Color gray_700 = Color(0xFF48484A);
+  static const Color gray_800 = Color(0xFF3A3A3C);
+  static const Color gray_800_20 = Color(0x332F2F2F);
+  static const Color gray_900 = Color(0xFF1C1C1E);
+  static const Color gray_900_50 = Color(0x801C1C1E);
+
+  static const Color red = Color(0xFFFF5B5B);
+  static const Color yellow = Color(0x801C1C1E);
+  static const Color green = Color(0xFF70BE69);
+  static const Color blue = Color(0xFF5B89FF);
+}
---
+++ ody_flutter/assets/color/light_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class LightColors {
+  static const Color first = CommonColors.cream;
+  static const Color second = CommonColors.purple_300;
+  static const Color third = CommonColors.purple_800;
+  static const Color fourth = CommonColors.white;
+  static const Color fifth = CommonColors.black;
+  static const Color sixth = CommonColors.gray_500;
+  static const Color seventh = CommonColors.gray_800;
+  static const Color eighth = CommonColors.gray_350;
+  static const Color ninth = CommonColors.cream;
+  static const Color tenth = CommonColors.white;
+  static const Color eleventh = CommonColors.gray_600;
+}
