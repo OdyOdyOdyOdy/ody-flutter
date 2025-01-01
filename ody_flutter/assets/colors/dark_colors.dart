d8b8feb1172e56aa77ea1f728389a2cbad72f44c
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-01-01 18:36:38 +0900

    setting: 글꼴, 색상, 이미지 추가 (#9)
    * setting #7: 필요한 색상들 추가
    
    * setting #7: 필요한 폰트들 추가
    
    * setting #7: 필요한 이미지들 추가
    
    * setting #7: color 패키지명 colors로 변경

---
+++ ody_flutter/assets/colors/dark_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class DarkColors {
+  static const Color first = CommonColors.black;
+  static const Color second = CommonColors.purple_800;
+  static const Color third = CommonColors.purple_300;
+  static const Color fourth = CommonColors.white;
+  static const Color fifth = CommonColors.cream;
+  static const Color sixth = CommonColors.gray_350;
+  static const Color seventh = CommonColors.cream;
+  static const Color eighth = CommonColors.gray_500;
+  static const Color ninth = CommonColors.gray_900;
+  static const Color tenth = CommonColors.black;
+  static const Color eleventh = CommonColors.gray_500;
+}
