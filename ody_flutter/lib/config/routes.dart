123f2c7455d73b8ad2a23df85e4ea3391833b04f
Joon Kyung Lee <haeumueah@gmail.com>
2025-01-16 14:46:25 +0900

    setting: 네비게이터 경로 상수화 (#42)
    * setting #35: 네비게이터 route 상수화
    
    * setting #35: 상수화된 route 적용

---
+++ ody_flutter/lib/config/routes.dart
@@ -0,0 +1,31 @@
+import "package:flutter/cupertino.dart";
+import "package:ody_flutter/screens/eta_board_screen.dart";
+import "package:ody_flutter/screens/gathering_creator_screen.dart";
+import "package:ody_flutter/screens/gatherings_screen.dart";
+import "package:ody_flutter/screens/invitation_code_screen.dart";
+import "package:ody_flutter/screens/login_screen.dart";
+import "package:ody_flutter/screens/settings_screen.dart";
+import "package:ody_flutter/screens/splash_screen.dart";
+import "package:ody_flutter/screens/status_board_screen.dart";
+
+Map<String, WidgetBuilder> namedRoutes = <String, WidgetBuilder>{
+  "/splash": (final BuildContext context) => const SplashScreen(),
+  "/login": (final BuildContext context) => const LoginScreen(),
+  "/gatherings": (final BuildContext context) => const GatheringsScreen(),
+  "/gatheringCreation": (final BuildContext context) => const GatheringCreatorScreen(),
+  "/settings": (final BuildContext context) => const SettingsScreen(),
+  "/invitationCode": (final BuildContext context) => const InvitationCodeScreen(),
+  "/etaBoard": (final BuildContext context) => const EtaBoardScreen(),
+  "/statusBoard": (final BuildContext context) => const StatusBoardScreen(),
+};
+
+class Routes {
+  static const String splash = "/splash";
+  static const String login = "/login";
+  static const String gatherings = "/gatherings";
+  static const String gatheringCreation = "/gatheringCreation";
+  static const String settings = "/settings";
+  static const String invitationCode = "/invitationCode";
+  static const String etaBoard = "/etaBoard";
+  static const String statusBoard = "/statusBoard";
+}
