2f54a327936e0c731204b9f545046c4b066a5356
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-01-16 11:47:31 +0900

    chore #33: 코드리뷰 반영
    - 불필요한 Container 제거

--- ody_flutter/lib/screens/splash_screen.dart
+++ ody_flutter/lib/screens/splash_screen.dart
@@ -15,14 +15,10 @@ class SplashScreen extends StatelessWidget {
     });
 
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: CommonColors.purple_800,
-        child: Center(
+      backgroundColor: CommonColors.purple_800,
+      body: Center(
           child: SvgPicture.asset(CommonImages.icSplashLogo),
         ),
-      ),
     );
   }
 }
