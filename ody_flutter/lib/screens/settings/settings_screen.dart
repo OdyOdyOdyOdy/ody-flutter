47caa6de4386ab3a929ddb837fb5eded491b2d8c
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-03-11 10:50:39 +0900

    feat #62: 개인 정보 처리 방침, 이용 약관 동의 연결 구현

--- ody_flutter/lib/screens/settings/settings_screen.dart
+++ ody_flutter/lib/screens/settings/settings_screen.dart
@@ -6,6 +6,7 @@ import "package:ody_flutter/assets/images/images.dart";
 import "package:ody_flutter/components/ody_top_bar.dart";
 import "package:ody_flutter/screens/settings/model/notification_setting.dart";
 import "package:ody_flutter/screens/settings/model/use_of_service_setting.dart";
+import "package:webview_flutter/webview_flutter.dart";
 
 final List<NotificationSetting> notificationSettings =
     NotificationSetting.values.cast<NotificationSetting>();
@@ -20,107 +21,166 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  bool _showWebView = false;
+  late WebViewController _webViewController;
+
   @override
-  Widget build(final BuildContext context) => Scaffold(
+  void initState() {
+    super.initState();
+    _webViewController = WebViewController();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
         backgroundColor: CommonColors.cream,
         body: SafeArea(
-          child: Column(
-            children: [
-              OdyTopBar(
-                title: "설정",
-                leftIcon: CommonImages.icArrowBack,
-                onLeftIcon: () {
-                  Navigator.pop(context);
-                },
-              ),
-              const SizedBox(height: 26),
-              Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "알림",
-                    style: PretendardFonts.bold18.copyWith(
-                      color: CommonColors.gray_600,
+          child: _showWebView
+              ? Scaffold(
+                  appBar: AppBar(
+                    leading: IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: _handleWebViewBack,
                     ),
+                    backgroundColor: CommonColors.white,
                   ),
-                ),
-              ),
-              const SizedBox(height: 10),
-              for (var i = 0; i < notificationSettings.length; i++)
-                Container(
-                  margin: const EdgeInsets.only(top: 16, bottom: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const SizedBox(width: 30),
-                          SvgPicture.asset(notificationSettings[i].icon),
-                          const SizedBox(width: 16),
-                          Text(
-                            notificationSettings[i].description,
-                            style: PretendardFonts.medium18.copyWith(
-                              color: CommonColors.gray_900,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(right: 30),
-                        child: SizedBox(
-                          width: 39,
-                          height: 24,
-                          child: Switch(
-                            value: false,
-                            onChanged: null,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              const SizedBox(height: 14),
-              const Divider(
-                color: CommonColors.gray_350,
-                thickness: 1,
-                height: 1,
-              ),
-              const SizedBox(height: 36),
-              Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "서비스 이용",
-                    style: PretendardFonts.bold18.copyWith(
-                      color: CommonColors.gray_600,
+                  body: WebViewWidget(controller: _webViewController),
+                )
+              : Column(
+                  children: [
+                    OdyTopBar(
+                      title: "설정",
+                      leftIcon: CommonImages.icArrowBack,
+                      onLeftIcon: () => Navigator.pop(context),
                     ),
-                  ),
+                    const SizedBox(height: 26),
+                    _buildNotificationSettings(),
+                    const SizedBox(height: 14),
+                    const Divider(
+                      color: CommonColors.gray_350,
+                      thickness: 1,
+                      height: 1,
+                    ),
+                    const SizedBox(height: 36),
+                    _buildServiceSettings(),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 26),
-              for (var i = 0; i < useOfServiceSettings.length; i++)
-                Container(
-                  margin: const EdgeInsets.only(top: 16, bottom: 16),
-                  child: Row(
-                    children: [
-                      const SizedBox(width: 30),
-                      SvgPicture.asset(useOfServiceSettings[i].icon),
-                      const SizedBox(width: 16),
-                      GestureDetector(
-                        child: Text(
-                          useOfServiceSettings[i].description,
-                          style: PretendardFonts.medium18.copyWith(
-                            color: CommonColors.gray_900,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-            ],
-          ),
         ),
       );
+
+  Widget _buildNotificationSettings() => Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "알림",
+                style: PretendardFonts.bold18.copyWith(
+                  color: CommonColors.gray_600,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          for (final setting in notificationSettings)
+            Container(
+              margin: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const SizedBox(width: 30),
+                      SvgPicture.asset(setting.icon),
+                      const SizedBox(width: 16),
+                      Text(
+                        setting.description,
+                        style: PretendardFonts.medium18.copyWith(
+                          color: CommonColors.gray_900,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 30),
+                    child: SizedBox(
+                      width: 39,
+                      height: 24,
+                      child: Switch(
+                        value: false,
+                        onChanged: null,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      );
+
+  Widget _buildServiceSettings() => Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "서비스 이용",
+                style: PretendardFonts.bold18.copyWith(
+                  color: CommonColors.gray_600,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 26),
+          for (final setting in useOfServiceSettings)
+            Container(
+              margin: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Row(
+                children: [
+                  const SizedBox(width: 30),
+                  SvgPicture.asset(setting.icon),
+                  const SizedBox(width: 16),
+                  GestureDetector(
+                    onTap: () async => _handleServiceItemTap(setting),
+                    child: Text(
+                      setting.description,
+                      style: PretendardFonts.medium18.copyWith(
+                        color: CommonColors.gray_900,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      );
+
+  Future<void> _handleServiceItemTap(UseOfServiceSetting setting) async {
+    _handleWebViewBack();
+    switch (setting.useOfServiceType) {
+      case UseOfServiceType.privacyPolicy:
+        await _webViewController.loadRequest(
+          Uri.parse(
+            "https://sly-face-106.notion.site/fecbe589eb23471ba2d0685cb3c2d274?pvs=4",
+          ),
+        );
+      case UseOfServiceType.term:
+        await _webViewController.loadRequest(
+          Uri.parse(
+            "https://sly-face-106.notion.site/beb204b6e6724ecbbb83496448fc7b53?pvs=4",
+          ),
+        );
+      case UseOfServiceType.logout:
+      case UseOfServiceType.withdraw:
+        // Handle logout or withdraw actions here
+        break;
+    }
+  }
+
+  void _handleWebViewBack() {
+    setState(() {
+      _showWebView = !_showWebView;
+    });
+  }
 }
