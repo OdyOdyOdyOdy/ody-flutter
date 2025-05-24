aa035ce51837380170a47d5bc18ddb35128f5a64
Joon Kyung Lee <haeumueah@gmail.com>
2025-05-24 12:29:11 +0200

    feat: 약속 목록 조회 (#77)
    * feat #71: 약속 목록 불러오기 구현
    
    * chore #71: lint

--- ody_flutter/lib/domain/repository/gathering_repository.dart
+++ ody_flutter/lib/domain/repository/gathering_repository.dart
@@ -1,6 +1,8 @@
 import "package:ody_flutter/domain/model/gathering.dart";
+import "package:ody_flutter/domain/model/gathering2.dart";
 
 abstract class GatheringRepository {
   Future<Gathering> createGathering(Gathering request);
   Future<Gathering> fetchGathering(int id);
+  Future<List<Gathering2>> fetchGatherings();
 }
--- ody_flutter/lib/screens/gathering_creator/gathering_creator_screen.dart
+++ ody_flutter/lib/screens/gathering_creator/gathering_creator_screen.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:ody_flutter/assets/colors/colors.dart";
 import "package:ody_flutter/assets/images/images.dart";
 import "package:ody_flutter/components/ody_top_bar.dart";
+import "package:ody_flutter/data/db/service/auth_token_service.dart";
 import "package:ody_flutter/data/network/base/base_service.dart";
 import "package:ody_flutter/data/network/service/gathering_service_impl.dart";
 import "package:ody_flutter/data/repository/gathering_repository_impl.dart";
@@ -17,7 +18,10 @@ class GatheringCreatorScreen extends StatelessWidget {
 
   // to do: 추후에 주입 필요
   final _viewModel = GatheringCreatorViewModel(
-    GatheringRepositoryImpl(GatheringService(BaseService())),
+    GatheringRepositoryImpl(
+      GatheringService(BaseService()),
+      AuthTokenService(),
+    ),
   );
 
   final _pageController = PageController();
