ba93e5c64efc973bb673ef8ca6fbb430f086bb22
김영인 <0inn1220@gmail.com>
2025-05-24 22:38:48 +0900

    chore #76: OdyTimePicker 위젯 수정

--- ody_flutter/lib/components/ody_time_picker.dart
+++ ody_flutter/lib/components/ody_time_picker.dart
@@ -1,37 +1,50 @@
 import "package:flutter/cupertino.dart";
-import "package:flutter/material.dart";
 import "package:ody_flutter/assets/colors/colors.dart";
 import "package:ody_flutter/assets/fonts/pretendard_fonts.dart";
 
 class OdyTimePicker extends StatefulWidget {
-  const OdyTimePicker({super.key});
+  const OdyTimePicker({
+    required this.selectedHour,
+    required this.selectedMinute,
+    super.key,
+  });
+
+  final ValueNotifier<int> selectedHour;
+  final ValueNotifier<int> selectedMinute;
 
   @override
   State<OdyTimePicker> createState() => _OdyTimePickerState();
 }
 
 class _OdyTimePickerState extends State<OdyTimePicker> {
-  int selectedHour = TimeOfDay.now().hour;
-  int selectedMinute = TimeOfDay.now().minute;
-
   @override
   Widget build(BuildContext context) => Row(
         mainAxisAlignment: MainAxisAlignment.center,
         spacing: 20,
         children: [
           _timerTextWidget(
-            List.generate(24, (index) => index.toString().padLeft(2, "0")),
-            selectedHour,
-            (value) => setState(() => selectedHour = int.parse(value)),
+            List.generate(
+              24,
+              (index) => index.toString().padLeft(2, "0"),
+            ),
+            widget.selectedHour.value,
+            (value) => setState(
+              () => widget.selectedHour.value = int.parse(value),
+            ),
           ),
           const Text(
             " : ",
             style: PretendardFonts.bold24,
           ),
           _timerTextWidget(
-            List.generate(60, (index) => index.toString().padLeft(2, "0")),
-            selectedMinute,
-            (value) => setState(() => selectedMinute = int.parse(value)),
+            List.generate(
+              60,
+              (index) => index.toString().padLeft(2, "0"),
+            ),
+            widget.selectedMinute.value,
+            (value) => setState(
+              () => widget.selectedMinute.value = int.parse(value),
+            ),
           ),
         ],
       );
