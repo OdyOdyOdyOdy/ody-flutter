210086d395a86199965a7a7f204840f8eee88dcf
김영인 <0inn1220@gmail.com>
2025-08-17 23:20:30 +0900

    feat #80: 약속 참여하기 api 연결

--- ody_flutter/lib/config/routes.dart
+++ ody_flutter/lib/config/routes.dart
@@ -30,7 +30,10 @@ Map<String, WidgetBuilder> namedRoutes = <String, WidgetBuilder>{
   "/statusBoard": (final BuildContext context) => const StatusBoardScreen(),
   "/gatheringLocationSearch": (final BuildContext context) =>
       GatheringLocationSearchScreen(),
-  "/gatheringEnter": (final BuildContext context) => GatheringEnterScreen(),
+  "/gatheringEnter": (
+    final BuildContext context,
+  ) =>
+      GatheringEnterScreen(),
   "/gatheringEnterComplete": (final BuildContext context) =>
       const GatheringEnterCompleteScreen(),
 };
