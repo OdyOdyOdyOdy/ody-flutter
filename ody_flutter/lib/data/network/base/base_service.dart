d2d3cfb06e4e1f7ce66285f399934115481b7ee2
haeum808 <159797292+haeum808@users.noreply.github.com>
2025-09-21 16:12:19 +0200

    feat #65: 리프레쉬 토큰 로직 구현

--- ody_flutter/lib/data/network/base/base_service.dart
+++ ody_flutter/lib/data/network/base/base_service.dart
@@ -2,7 +2,6 @@ import "dart:convert";
 
 import "package:dio/dio.dart";
 import "package:flutter/foundation.dart";
-import "package:flutter_dotenv/flutter_dotenv.dart";
 import "package:injectable/injectable.dart";
 import "package:ody_flutter/data/db/service/auth_token_service.dart";
 import "package:ody_flutter/data/network/base/base_exception.dart";
@@ -10,35 +9,10 @@ import "package:ody_flutter/domain/model/auth_token.dart";
 
 @injectable
 class BaseService {
-  BaseService(this.authTokenService) {
-    _setupInterceptors();
-  }
+  BaseService(this.authTokenService, this._dio);
 
   final AuthTokenService authTokenService;
-
-  final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: dotenv.get("BASE_DEV_URL"),
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
-      contentType: Headers.jsonContentType,
-    ),
-  );
-
-  void _setupInterceptors() {
-    _dio.interceptors.add(
-      InterceptorsWrapper(
-        onResponse: (response, handler) {
-          _printSuccessLog(response);
-          handler.next(response);
-        },
-        onError: (error, handler) {
-          _printErrorLog(error);
-          handler.next(error);
-        },
-      ),
-    );
-  }
+  final Dio _dio;
 
   Future<AuthToken?> _getStoredToken() => authTokenService.getToken();
 
@@ -164,52 +138,6 @@ class BaseService {
         throw BaseException.unexpectedError(
           message: "${e.message}",
         );
-    }
-  }
-
-  void _printSuccessLog(Response response) {
-    debugPrint("""
-=== [Network Success] ======================================
-🟢 Request Info:
-  • URL: ${response.requestOptions.uri}
-  • Method: ${response.requestOptions.method}
-  • Headers: ${_formatJson(response.requestOptions.headers)}
-  • Data: ${_formatJson(response.requestOptions.data)}
-
-✅ Response Info:
-  • Status Code: ${response.statusCode}
-  • Status Message: ${response.statusMessage}
-  • Response Data: ${_formatJson(response.data)}
-============================================================
-""");
-  }
-
-  void _printErrorLog(DioException e) {
-    debugPrint("""
-=== [Network Error] =========================================
-🔴 Request Info:
-  • URL: ${e.requestOptions.uri}
-  • Method: ${e.requestOptions.method}
-  • Headers: ${_formatJson(e.requestOptions.headers)}
-  • Data: ${_formatJson(e.requestOptions.data)}
-
-❌ Response Error:
-  • Status Code: ${e.response?.statusCode}
-  • Status Message: ${e.response?.statusMessage}
-  • Response Data: ${_formatJson(e.response?.data)}
-
-🗯️ Error Message:
-  ${e.message}
-============================================================
-""");
-  }
-
-  String _formatJson(json) {
-    const encoder = JsonEncoder.withIndent("  ");
-    try {
-      return encoder.convert(json);
-    } on Exception catch (_) {
-      return json.toString();
     }
   }
 }
---
+++ ody_flutter/lib/data/network/interceptor/refresh_token_interceptor.dart
@@ -0,0 +1,37 @@
+import "package:dio/dio.dart";
+import "package:ody_flutter/domain/repository/auth_repository.dart";
+import "package:synchronized/synchronized.dart";
+
+class RefreshTokenInterceptor extends QueuedInterceptorsWrapper {
+  RefreshTokenInterceptor({
+    required this.authRepository,
+    required this.dio,
+  });
+
+  final AuthRepository authRepository;
+  final Dio dio;
+  final _lock = Lock();
+
+  @override
+  Future<void> onError(
+      DioException err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401) {
+      await _lock.synchronized(() async {
+        try {
+          final newAuthToken = await authRepository.postRefreshToken();
+
+          final requestOptions = err.requestOptions;
+          requestOptions.headers["Authorization"] =
+              "Bearer access-token=${newAuthToken.accessToken}";
+
+          final response = await dio.fetch(requestOptions);
+          return handler.resolve(response);
+        } on DioException catch (e) {
+          return handler.reject(e);
+        }
+      });
+    } else {
+      return handler.next(err);
+    }
+  }
+}
