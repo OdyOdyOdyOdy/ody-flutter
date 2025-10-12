bf33fe5972137a3db1215ad6b7a808398a4dc64f
김영인 <0inn1220@gmail.com>
2025-10-12 18:31:40 +0900

    chore #113: location 관련 response 수정
    * x, y 값이 double, string으로 내려와 별도 response 생성함

--- ody_flutter/lib/data/entity/gathering/location_response.dart
+++ ody_flutter/lib/data/entity/gathering/location_response.dart
@@ -16,20 +16,45 @@ class LocationResponse {
 class LocationResponseData {
   LocationResponseData(
     this.placeName,
-    this.roadAddressName,
+    this.addressName,
     this.x,
     this.y,
   );
+
   factory LocationResponseData.fromJson(Map<String, dynamic> json) =>
       LocationResponseData(
-        json["place_name"],
-        json["road_address_name"],
-        json["x"],
-        json["y"],
+        json["place_name"] as String?,
+        json["address_name"] as String?,
+        json["x"] as double?,
+        json["y"] as double?,
       );
 
   String? placeName;
-  String? roadAddressName;
+  String? addressName;
+  double? x;
+  double? y;
+}
+
+class LocationResponseDataWithStringCoord {
+  LocationResponseDataWithStringCoord(
+    this.placeName,
+    this.addressName,
+    this.x,
+    this.y,
+  );
+
+  factory LocationResponseDataWithStringCoord.fromJson(
+    Map<String, dynamic> json,
+  ) =>
+      LocationResponseDataWithStringCoord(
+        json["place_name"] as String?,
+        json["address_name"] as String?,
+        json["x"] as String?,
+        json["y"] as String?,
+      );
+
+  String? placeName;
+  String? addressName;
   String? x;
   String? y;
 }
